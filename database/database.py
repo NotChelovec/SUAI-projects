c2cca4ff2b59adf9e045296e3d799cf47c1088e7
NotChelovec <project.which.i.do@gmail.com>
2024-11-09 02:34:58 +0300

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,1 +1,21 @@
+import os 
+from sqlalchemy import create_engine, Engine, event
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from dotenv import load_dotenv
+
+load_dotenv()
+
+engine = create_engine (f'sqlite:///{os.getenv("DATABASE_NAME")}.slite')
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+class Base(DeclarativeBase):
+    pass
+
+def create_tables():
+    Base.metadata.create_all(engine)
+    
 
